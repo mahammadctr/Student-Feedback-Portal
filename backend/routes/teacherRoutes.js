a1e6180d7d424ad0cb9aec769b35ad3ae16d39bb
MAHAMMAD HUSSAIN <mahammadhussain@MAHAMMADs-MacBook-Air.local>
2025-08-10 12:30:20 +0530

    Added project files

---
+++ backend/routes/teacherRoutes.js
@@ -0,0 +1,55 @@
+const express=require('express');
+const Teacher=require('../models/Teacher');
+
+const router=express.Router();
+
+router.post('/register',async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+       //console.log('hitting teacher')
+
+       if(!email.endsWith('@iiitl.ac.in'))
+      {
+        throw Error("Please register with institutes email id");
+      }
+      
+      const teacher = new Teacher({
+        name,
+        email,
+        password
+      });
+  
+      await teacher.save();
+      res.status(200).json({ teacher });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  })
+
+  
+router.post('/login',async(req,res)=>{
+
+  try{
+
+   // console.log("hitting login teacher")
+    const {email,password}=req.body;
+    
+    const teacher=await Teacher.findOne({email,password})
+
+    if(!teacher){
+      throw Error("email or password is wrong");
+    }
+
+    res.status(200).json({teacher});
+
+  }
+  catch(err){
+    res.status(500).json({message:err.message})
+  }
+})
+  
+
+  
+module.exports=router;
+
+
---
+++ backend/server.js
@@ -0,0 +1,144 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const fetch = require("node-fetch");
+const dotenv =require('dotenv');
+dotenv.config();
+
+const Student = require("./models/Student");
+const Teacher = require("./models/Teacher");
+const Question = require("./models/Question");
+const Feedback = require("./models/Feedback");
+const Submission = require("./models/Submission");
+const Notification = require("./models/Notification");
+const Classes = require("./models/Classes");
+
+const studentRoutes = require("./routes/studentRoutes");
+const teacherRoutes = require("./routes/teacherRoutes");
+const feedbackRoutes = require("./routes/feedbackRoutes");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'))
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    process.env.MONGODB_URL
+  )
+  .then(() => {
+    console.log("connected to backend");
+  })
+  .catch((err) => {
+    console.log("error connecting database", err);
+  });
+
+app.use("/student", studentRoutes);
+
+app.use("/teacher", teacherRoutes);
+
+app.use("/feedback", feedbackRoutes);
+
+app.get("/deleteAllData", async (req, res) => {
+  try {
+    await Student.deleteMany({});
+    await Question.deleteMany({});
+    await Teacher.deleteMany({});
+    await Submission.deleteMany({});
+    await Feedback.deleteMany({});
+    await Notification.deleteMany({});
+
+    res.status(200).json({ message: "deleted all data from database" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.get("/allNotifications", async (req, res) => {
+  try {
+    const allNotifications = await Notification.find({})
+      .sort({ createdAt: -1 })
+      .limit(7);
+
+    res.status(200).json({ notifications: allNotifications });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.post("/notification", async (req, res) => {
+  // console.log("hitting notf")
+
+  const message = "notification created succesfully";
+  const notification = new Notification({ message });
+
+  res.status(200).json({ notification });
+});
+
+app.post("/createClass", async (req, res) => {
+  //below , instead of CSAI20 .take value from req.body
+  const getBranch = req.body;
+  const branch = getBranch.branch;
+  const newClass = new Classes({ branch });
+  //console.log(branch)
+
+  await newClass.save();
+  // console.log(newClass)
+  res.status(200).json({ msg: "ok" });
+});
+
+app.get("/getAllClasses", async (req, res) => {
+  const classes = await Classes.find({});
+  res.status(200).json({ classes });
+});
+
+/*  app.get('/checkFetch',async(req,res)=>{
+
+    const sendReq=async(datas)=>
+    {
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/knkarthick/MEETING_SUMMARY",
+            {
+                headers: { Authorization: `Bearer REDACTED` },
+                method: "POST",
+                body: JSON.stringify(datas),
+            }
+        );
+        const result = await response.json();
+       
+         console.log(result)
+
+    }
+     
+    sendReq(datas);
+    res.status(200).send({message:'ok'});
+
+  }) */
+
+app.get("/dataForDashboard", async (req, res) => {
+  try {
+    const teachers = await Teacher.find({});
+    const students = await Student.find({});
+
+    var student_count = 0;
+
+    for (let stu of students) {
+      student_count += 1;
+    }
+
+    res.status(200).json({ teachers, student_count });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.listen(3005, (req, res) => {
+  console.log("listening on port 3005");
+});
